9ec0d4bf201bd965d81890712208dc496e63e240
elias <elias@eliass-MacBook-Pro.local>
2025-11-30 12:30:10 +0100

    reformat

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-import os
-
+from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -12,6 +11,6 @@ app = FastAPI()
 app.include_router(router)
 
 
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-static_dir = os.path.join(base_dir, "static")
+base_dir = Path(__file__).resolve().parent.parent
+static_dir = base_dir / "static"
 app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
